7ce1fd3c222a7bb97e4ad6a659021953fdc93727
Endity <enditywasita@gmailcom>
2021-03-29 20:48:14 +0700

    added function get_data, edit_data, update, and delete

--- application/models/Control_Model.php
+++ application/models/Control_Model.php
@@ -10,6 +10,24 @@ class Control_Model extends CI_Model
   {
     return $this->db->query("SELECT * FROM mahasiswa WHERE email = '$email' AND password = '$password'")->result();
   }
+  function get_data()
+  {
+    return $this->db->query("SELECT * FROM mahasiswa")->result();
+  }
+  function edit_data($id)
+  {
+    return $this->db->query("SELECT * FROM mahasiswa WHERE id = '$id'")->result();
+  }
+  function update($where,$table,$data)
+  {
+    $this->db->where($where);
+    $this->db->update($table,$data);
+  }
+  function delete($where,$table)
+  {
+    $this->db->where($where);
+    $this->db->delete($table);
+  }
 }
 
  ?>
