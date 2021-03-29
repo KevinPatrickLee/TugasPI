52c506123c66aee3f46b1d5658f90b6ced4df95c
Endity <enditywasita@gmailcom>
2021-03-29 18:46:31 +0700

    added NIM and added action and method in form

--- application/views/register.php
+++ application/views/register.php
@@ -3,13 +3,16 @@
   <title>Register</title>
 </head>
 <body>
-  <form>
+  <form action="<?php echo base_url('control/login'); ?>" method="post">
   <h1>Register</h1>
+  <h3>NIM</h3>
+  <input type="number" placeholder="Enter Name" name="name" id="nim" required>
+
   <h3>Name</h3>
   <input type="text" placeholder="Enter Name" name="name" id="name" required>
 
   <h3>Email</h3>
-  <input type="text" placeholder="Enter Email" name="email" id="email" required>
+  <input type="email" placeholder="Enter Email" name="email" id="email" required>
 
   <h3>Password</h3>
   <input type="password" placeholder="Enter Password" name="psw" id="psw" required>
